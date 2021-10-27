702e85b011acdfb3544399f18a7c8f0befa276fe
[dcquinche] <[dcquinche@gmail.com]>
2021-10-27 17:21:22 -0500

    cambio en usuarios

--- src/pages/GestionUsuarios.jsx
+++ src/pages/GestionUsuarios.jsx
@@ -302,8 +302,8 @@ const FilaUsuario = ({usuarios, setConsulta, getToken})=>{
             <td>
             <select value={infoEditarUsuario.rol} onChange={(e)=>setInfoEditarUsuario({...infoEditarUsuario, rol:e.target.value})} >
                 <option disabled value={0}>Seleccione una opción</option>
-                <option value="vendedor">Vendedor</option>
-                <option value="administrador">Administrador</option>
+                <option >Vendedor</option>
+                <option >Administrador</option>
             </select>
             </td>
             </>
